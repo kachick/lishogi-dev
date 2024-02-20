c9668d0c3b2fda1e25b35e4308077c03b449704b
Kenichi Kamiya <kachick1@gmail.com>
2024-02-21 01:35:50 +0900

    Start to consider lishogi-db-scripts

--- scripts/pin_repos.bash
+++ scripts/pin_repos.bash
@@ -5,3 +5,4 @@ set -euxo pipefail
 cd ./repos/lishogi && git checkout a664c1909115b3f5fd427f510d1634335d0b2c1d && cd ../..
 cd ./repos/lila-ws && git checkout 60e3a2b02e11c4bb1ce4bfe2d3cb844486a884c5 && cd ../..
 cd ./repos/shoginet && git checkout 9d2d5244b5de9e1076deb04d598c4efc13ac6d21 && cd ../..
+cd ./repos/lishogi-db-scripts && git checkout daffb4f68654f894c02da02ad648ef6739b9d02f && cd ../..
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -5,3 +5,4 @@ set -euxo pipefail
 [ -d "./repos/lishogi" ] || git -C ./repos clone git@github.com:WandererXII/lishogi.git
 [ -d "./repos/lila-ws" ] || git -C ./repos clone git@github.com:WandererXII/lila-ws.git
 [ -d "./repos/shoginet" ] || git -C ./repos clone git@github.com:WandererXII/shoginet.git
+[ -d "./repos/lishogi-db-scripts" ] || git -C ./repos clone git@github.com:WandererXII/lishogi-db-scripts.git
