61e5165c84731a2d18f3ad813b2abcc58a716b32
Kenichi Kamiya <kachick1@gmail.com>
2025-05-13 01:02:46 +0900

    Update channel and style

--- flake.nix
+++ flake.nix
@@ -1,50 +1,51 @@
 {
   inputs = {
-    # Candidate channels
-    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
-    # How to update the revision
-    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              bashInteractive
-
-              # Maintain this repo
-              nil
-              nixfmt-rfc-style
-              dprint
-              typos
-              go-task
-              shellcheck
-              shfmt
-              hostname
-            ];
-
-            shellHook = ''
-              echo 'Happy coding! Happy shogi!'
-            '';
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            buildInputs = (
+              with pkgs;
+              [
+                # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+                bashInteractive
+                # Maintain this repo
+                nil
+                nixfmt-rfc-style
+                dprint
+                typos
+                go-task
+                shellcheck
+                shfmt
+                hostname
+              ]
+            );
           };
-
-        packages.lila = pkgs.mkShell {
+        }
+      );
+      packages.lila = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShell {
           buildInputs = with pkgs; [
             bashInteractive
 
@@ -57,7 +58,7 @@
           shellHook = ''
             echo 'Dev shell for "lila"'
           '';
-        };
-      }
-    );
+        }
+      );
+    };
 }
